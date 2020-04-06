e2ec6e4d0378279b4ae89064510be725c2b7888a
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-04-06 15:25:52 -0600

    Creating SMS message linked to user desired notification day.

--- WeekendWindow/APIKEYS.cs
+++ WeekendWindow/APIKEYS.cs
@@ -10,6 +10,6 @@ namespace WeekendWindow
         public static readonly string TwilioToden = "0726aab68766dee6457b5b2efba4be93";
         public static readonly string TwilioSid = "ACc0f1430439ef148f248ad7935e58ce62";
         public static readonly string TwilioPhoneNumber = "19135218316";
-        public static readonly string TwilioTestNumber = "3607204065";
+        public static readonly string TwilioTestNumber = "";
     }
 }
