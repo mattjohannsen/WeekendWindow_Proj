77d7f98ee595ced399a8ba9dcdb3d1eb51de2437
mattjohannsen <majohann@hotmail.com>
2020-04-08 16:48:07 -0500

    Added SendSMS method to the AdminsController, also installed Twilio package in Visual Studio

--- WeekendWindow/Controllers/AdminsController.cs
+++ WeekendWindow/Controllers/AdminsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 using WeekendWindow.Data;
 using WeekendWindow.Models;
 
@@ -155,6 +157,22 @@ namespace WeekendWindow.Controllers
         private bool AdminExists(int id)
         {
             return _context.Admins.Any(e => e.AdminId == id);
+        }
+
+        public async Task SendSms()
+        {
+            var currentDay = DateTime.Today.DayOfWeek.GetHashCode();
+            var viewersList = _context.Viewers.Where(v => v.NotificationDay == currentDay).ToList();
+            const string accountSid = APIKEYS.TwilioSid;
+            const string authToken = APIKEYS.TwilioToken;
+            string phoneNumber = "+13607204065";
+            TwilioClient.Init(accountSid, authToken);
+            var message = await MessageResource.CreateAsync(
+            body: "Time to set up your awesome weekend!!!",
+            from: new Twilio.Types.PhoneNumber("+19135218316"),
+            to: new Twilio.Types.PhoneNumber(phoneNumber)
+            );
+            Console.WriteLine(message);
         }
     }
 }
