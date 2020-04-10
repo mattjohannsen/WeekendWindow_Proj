55720af747bf8bde86fc822c21233916168434bd
mattjohannsen <majohann@hotmail.com>
2020-04-10 11:54:34 -0500

    Changed Viewers Index to redirect to Create if Viewer is null.

--- WeekendWindow/Controllers/ViewersController.cs
+++ WeekendWindow/Controllers/ViewersController.cs
@@ -32,49 +32,59 @@ namespace WeekendWindow.Controllers
         // GET: Viewers
         public async Task<IActionResult> Index()
         {
-            //var data = new List<WeatherForecast>();
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            Viewer viewer = _context.Viewers.Where(v => v.IdentityUserId == userId).FirstOrDefault();
+            if (viewer != null)
+            {
+                //var data = new List<WeatherForecast>();
 
-            WeatherForecast forecast = await _forecastRequest.GetWeatherForecast();
-            DateTime dt = DateTime.Today;
-            DayOfWeek dw = dt.DayOfWeek;
+                WeatherForecast forecast = await _forecastRequest.GetWeatherForecast();
+                DateTime dt = DateTime.Today;
+                DayOfWeek dw = dt.DayOfWeek;
 
-            int num = (int)DateTime.Today.DayOfWeek;
-            int num2 = (int)dw;
+                int num = (int)DateTime.Today.DayOfWeek;
+                int num2 = (int)dw;
 
-            DateTime satNum = DateTime.Today.AddDays(6 - num2);
-            DateTime sunNum = satNum.AddDays(1);
+                DateTime satNum = DateTime.Today.AddDays(6 - num2);
+                DateTime sunNum = satNum.AddDays(1);
 
-            string sat = satNum.ToString("yyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
-            string sun = sunNum.ToString("yyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
+                string sat = satNum.ToString("yyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
+                string sun = sunNum.ToString("yyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
 
-            var data = forecast.data.Where(d => (d.valid_date == sat) || (d.valid_date == sun)).ToList();
+                var data = forecast.data.Where(d => (d.valid_date == sat) || (d.valid_date == sun)).ToList();
 
-            ViewData["City"] = forecast.city_name;
-            ViewData["State"] = forecast.state_code;
+                ViewData["City"] = forecast.city_name;
+                ViewData["State"] = forecast.state_code;
 
-            //Saturday Weather
-            ViewData["SatDate"] = data[0].valid_date;
-            ViewData["SatHighTemp"] = data[0].high_temp;
-            ViewData["SatLowTemp"] = data[0].low_temp;
-            ViewData["SatWindSpeed"] = data[0].wind_spd;
-            ViewData["SatGustsTo"] = data[0].wind_gust_spd;
-            ViewData["SatWindDir"] = data[0].wind_dir;
-            ViewData["SatSkies"] = data[0].weather.description;
+                //Saturday Weather
+                ViewData["SatDate"] = data[0].valid_date;
+                ViewData["SatHighTemp"] = data[0].high_temp;
+                ViewData["SatLowTemp"] = data[0].low_temp;
+                ViewData["SatWindSpeed"] = data[0].wind_spd;
+                ViewData["SatGustsTo"] = data[0].wind_gust_spd;
+                ViewData["SatWindDir"] = data[0].wind_dir;
+                ViewData["SatSkies"] = data[0].weather.description;
 
-            //Sunday Weather
-            ViewData["SunDate"] = data[1].valid_date;
-            ViewData["SunHighTemp"] = data[1].high_temp;
-            ViewData["SunLowTemp"] = data[1].low_temp;
-            ViewData["SunWindSpeed"] = data[1].wind_spd;
-            ViewData["SunGustsTo"] = data[1].wind_gust_spd;
-            ViewData["SunWindDir"] = data[1].wind_dir;
-            ViewData["SunSkies"] = data[1].weather.description;
+                //Sunday Weather
+                ViewData["SunDate"] = data[1].valid_date;
+                ViewData["SunHighTemp"] = data[1].high_temp;
+                ViewData["SunLowTemp"] = data[1].low_temp;
+                ViewData["SunWindSpeed"] = data[1].wind_spd;
+                ViewData["SunGustsTo"] = data[1].wind_gust_spd;
+                ViewData["SunWindDir"] = data[1].wind_dir;
+                ViewData["SunSkies"] = data[1].weather.description;
 
 
-            return View(forecast);
+                return View(forecast);
 
-            //var applicationDbContext = _context.Viewers.Include(v => v.IdentityUser);
-            //return View(await applicationDbContext.ToListAsync());
+                //var applicationDbContext = _context.Viewers.Include(v => v.IdentityUser);
+                //return View(await applicationDbContext.ToListAsync());
+            }
+            else
+            {
+                return RedirectToAction(nameof(Create));
+            }
+
         }
 
         // GET: Viewers/Details/5
